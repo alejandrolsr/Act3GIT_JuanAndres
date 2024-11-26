6e19e43811378ec2954725d98cf50341d053acfe
alejandrolsr <alopsal2504@g.educaand.es>
2024-11-26 14:21:02 +0100

    Update NumeroAleatorios.java

--- NumeroAleatorios/src/NumeroAleatorios/NumeroAleatorios.java
+++ NumeroAleatorios/src/NumeroAleatorios/NumeroAleatorios.java
@@ -14,8 +14,8 @@ public class NumeroAleatorios {
     public static void mostrarNumerosAleatorios() {
         Random random = new Random(); // Instancia para generar números aleatorios
 
-        System.out.println("30 números aleatorios entre 1 y 10:");
-        for (int i = 0; i < 30; i++) {
+        System.out.println("50 números aleatorios entre 1 y 10:"); //Modifico el método de mi compañero Juan Andrés.
+        for (int i = 0; i < 50; i++) {
             int numero = random.nextInt(10) + 1; // Genera un número entre 1 y 10
             System.out.print(numero + " "); // Muestra el número en la misma línea
         }
